893dcebbecfd406d3dd9e965331f665ad8271253
Ben Tyler <btyler@hubspot.com>
2023-08-31 13:33:05 -0400

    sketch of auction configuration

--- src/app/extensions/AuctionConfigure.jsx
+++ src/app/extensions/AuctionConfigure.jsx
@@ -6,6 +6,8 @@ import {
   Text,
   Input,
   Stack,
+  DateInput,
+  NumberInput,
   hubspot,
 } from '@hubspot/ui-extensions';
 
@@ -20,13 +22,15 @@ hubspot.extend(({ context, runServerlessFunction, actions }) => (
 
 // Define the Extension component, taking in runServerless, context, & sendAlert as props
 const Extension = ({ context, runServerless, sendAlert }) => {
-  const [text, setText] = useState('');
+  const [minBids, setMinBids] = useState(0);
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
 
   // Call serverless function to execute with parameters.
   // The name `myFunc` as per configurations inside `serverless.json`
 
-  const submitBid = () => {
-    runServerless({ name: 'statGenerator', propertiesToSend: ['hs_object_id'], parameters: { text: text } }).then((resp) => {
+  const createAuction = () => {
+    runServerless({ name: 'auction-configure', propertiesToSend: ['hs_object_id'], parameters: { minBids: minBids, startTime: startTime, endTime: endTime } }).then((resp) => {
       console.log(resp);
       sendAlert({ message: resp.response })
     });
@@ -40,9 +44,33 @@ const Extension = ({ context, runServerless, sendAlert }) => {
         </Text>
       </Text>
       <Stack>
-        <Input name="text" label="Bid" onInput={(t) => setText(t)} />
-        <Button type="submit" onClick={submitBid}>
-          Click me
+        <DateInput
+          name="start-time"
+          label="Auction Start"
+          description="Auction Start Time"
+          onInput={(t) => {
+            console.log("START TIME", t);
+            setStartTime(t)
+          }}
+        />
+
+        <DateInput
+          name="end-time"
+          label="Auction End"
+          description="Auction End Time"
+          onInput={(t) => setEndTime(t)}
+        />
+
+        <NumberInput
+          name="min-bid-count"
+          label="Minimum bid count"
+          description="Minimum bids required for auction to close"
+          value="0"
+          onInput={(t) => setMinBids(t)}
+        />
+
+        <Button type="submit" onClick={createAuction}>
+          Create new Auction
         </Button>
       </Stack>
     </>

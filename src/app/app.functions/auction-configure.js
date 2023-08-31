765e6eb81adb10f943d62a63f8911567a5161f5b
Ben Tyler <btyler@hubspot.com>
2023-08-31 13:31:12 -0400

    add functions for auction setup

---
+++ src/app/app.functions/auction-configure.js
@@ -0,0 +1,12 @@
+const hubspot = require('@hubspot/api-client')
+
+exports.main = async (context = {}, sendResponse) => {
+  console.log('wharblgarbl', context);
+  const ret = `This is coming from a serverless function!`;
+  try {
+    sendResponse(ret);
+  } catch (error) {
+    sendResponse(error);
+  }
+};
+
---
+++ src/app/app.functions/get-auctions-for-item.js
@@ -0,0 +1,12 @@
+const hubspot = require('@hubspot/api-client')
+
+exports.main = async (context = {}, sendResponse) => {
+  console.log('get auctions for item', context);
+  const ret = `get auctions for item`;
+  try {
+    sendResponse(ret);
+  } catch (error) {
+    sendResponse(error);
+  }
+};
+

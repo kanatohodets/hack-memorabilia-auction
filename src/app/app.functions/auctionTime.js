170908fac7ff6904f6793ca998bb3839ac95fe18
Zach Probst <zprobst@hubspot.com>
2023-08-31 11:07:41 -0400

    getRemainingTime

--- src/app/app.functions/auctionTime.js
+++ src/app/app.functions/auctionTime.js
@@ -1,8 +1,21 @@
-const getRemainingTime = () => {
-  // TODO: fetch timestamp from auction object
-  return Date.now() + 10000
+const getRemainingTime = async (hubspotClient, playerId) => {
+  const BatchInputPublicFetchAssociationsBatchRequest = { inputs: [{"id": playerId }] };
+  const fromObjectType = "Player";
+  const toObjectType = "Item";
+
+  try {
+    const apiResponse = await hubspotClient.crm.associations.v4.batchApi.getPage(fromObjectType, toObjectType, BatchInputPublicFetchAssociationsBatchRequest);
+    console.log(JSON.stringify(apiResponse, null, 2));
+    return 10;
+  } catch (e) {
+    e.message === 'HTTP request failed'
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e)
+      return 0;
+  }
+
 };
 
-module.exports = {
+export default {
   getRemainingTime
 }

0e43fa0d6c46c0834d8d1b55a625edf27f030726
Ben Tyler <btyler@hubspot.com>
2023-08-31 16:48:54 -0400

    script to create a bid

---
+++ scripts/create-bid.mjs
@@ -0,0 +1,17 @@
+import { Client } from "@hubspot/api-client";
+const hubspotClient = new Client({ accessToken: process.env["ACCESS_TOKEN"] });
+
+const objectType = "p_bids";
+
+const properties = {
+  amount: "20543",
+  bid_id: "9999",
+};
+
+const associations = [];
+
+const apiResponse = await hubspotClient.crm.objects.basicApi.create(
+  objectType,
+  { properties: properties, associations: associations },
+);
+console.log(JSON.stringify(apiResponse));
